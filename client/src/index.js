60b30f67f6f776f3d7f69b59e29c6a2d365867f0
ShermanK78 <info@gteonline.co.za>
2023-10-23 10:21:37 +0200

    Uodated config

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const port = process.env.PORT || 9001;
+
 
 
 const root = createRoot(document.getElementById('root'));
