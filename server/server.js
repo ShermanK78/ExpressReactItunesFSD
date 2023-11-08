eefb67c06cdadc6ac86170fd36b3133897785ec2
ShermanK78 <140594633+ShermanK78@users.noreply.github.com>
2023-11-08 14:41:57 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,10 +28,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Server Running'); // Display "Server Running" message
-});
-
 app.get('/proxy-image', async (req, res) => {
   try {
     const imageUrl = req.query.url; // Get the image URL from the request query
